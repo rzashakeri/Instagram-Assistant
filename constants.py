7a19cb5dac8b8e9e031d0a53fc0317371daf6103
Reza Shakeri <rzashakeri@gmail.com>
2023-03-13 20:21:35 +0330

    ✨ feat: added DOWNLOAD_MEDIA, UPLOAD_MEDIA, LOGIN, INSIGHT, reply_keyboard constant

---
+++ constants.py
@@ -0,0 +1,10 @@
+DOWNLOAD_MEDIA: str = "download media"
+UPLOAD_MEDIA: str = "upload media"
+LOGIN: str = "login"
+INSIGHT: str = "Insights"
+reply_keyboard: list = [
+    [DOWNLOAD_MEDIA],
+    [INSIGHT],
+    [UPLOAD_MEDIA],
+    [LOGIN],
+]
