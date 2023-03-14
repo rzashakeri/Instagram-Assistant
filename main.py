2bc6330ff392af2799a9119d9dad2d9838c8abb4
Reza Shakeri <rzashakeri@gmail.com>
2023-03-14 13:41:48 +0330

    ♻️ refactor: refactored main.py structure

--- main.py
+++ main.py
@@ -1,42 +1,11 @@
-# coding: utf-8
-import os
-import signal
-import sys
-from importlib import import_module
-from telegram.ext import Dispatcher, Updater
+from telegram.ext import Application
 from configurations import settings
 from utils import logger
-
-
-def load_handlers(dispatcher: Dispatcher):
-    """Load handlers from files in a 'bot' directory."""
-    base_path = os.path.join(os.path.dirname(__file__), "bot")
-    files = os.listdir(base_path)
-    ignored_files = ["__init__.py", "__pycache__"]
-    for file_name in files:
-        if file_name not in ignored_files:
-            handler_module, _ = os.path.splitext(file_name)
-            module = import_module(f".{handler_module}", "bot")
-            module.init(dispatcher)
-
-
-def graceful_exit(*_, **__):
-    """Provide a graceful exit from a webhook server."""
-    if updater is not None:
-        updater.bot.delete_webhook()
-
-    sys.exit(0)
-
+from bot.conversation import conversation
 
 if __name__ == "__main__":
     logger.init_logger(f"logs/{settings.NAME}.log")
-
-    updater = Updater(token=settings.TOKEN)
-
-    load_handlers(updater.dispatcher)
-
-    if settings.WEBHOOK:
-        signal.signal(signal.SIGINT, graceful_exit)
-        updater.start_webhook(**settings.WEBHOOK_OPTIONS)
-    else:
-        updater.start_polling()
+    application = Application.builder().token(settings.TOKEN).build()
+    conversation_handler = conversation()
+    application.add_handler(conversation_handler)
+    application.run_polling()
