a34c9a0262d079df91d08e52c8758464a04a6450
Reza Shakeri <rzashakeri@gmail.com>
2023-03-14 13:40:14 +0330

    ✨ feat: added conversation handler

---
+++ bot/conversation.py
@@ -0,0 +1,32 @@
+# encoding: utf-8
+
+from logging import getLogger
+
+from telegram import Update
+from telegram.ext import (
+    CallbackContext,
+    CommandHandler,
+    ConversationHandler, MessageHandler, filters,
+)
+
+from bot.start import start
+from core.constants import CHOOSING, TYPING_CHOICE, TYPING_REPLY
+
+# Init logger
+logger = getLogger(__name__)
+
+
+def conversation():
+    """Process a /start command."""
+    conversation_handler = ConversationHandler(
+        entry_points=[CommandHandler("start", start)],
+        states={
+            CHOOSING: [
+
+            ],
+            TYPING_CHOICE: [],
+            TYPING_REPLY: [],
+        },
+        fallbacks=[],
+    )
+    return conversation_handler
