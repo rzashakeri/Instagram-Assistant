7480b7d9988f1b92546bad4a58935f66569cda0e
Reza Shakeri <rzashakeri@gmail.com>
2023-03-14 13:43:23 +0330

    ♻️ refactor: refactored constant values

--- core/constants.py
+++ core/constants.py
@@ -1,5 +1,5 @@
-DOWNLOAD_MEDIA: str = "Download Media 📥"
-UPLOAD_MEDIA: str = "Upload Media 📤"
-LOGIN: str = "Login 🔐"
-INSIGHT: str = "Insights 📈"
+DOWNLOAD_MEDIA: str = "Download Media"
+UPLOAD_MEDIA: str = "Upload Media"
+LOGIN: str = "Login"
+INSIGHT: str = "Insights"
 CHOOSING, TYPING_REPLY, TYPING_CHOICE = range(3)
