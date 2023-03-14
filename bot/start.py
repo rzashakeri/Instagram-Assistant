fbb6b430826878575e3f04c96dcdfc8367c46414
Reza Shakeri <rzashakeri@gmail.com>
2023-03-14 11:07:32 +0330

    ♻️ refactor: changed welcome message of start command and optimized imports

--- bot/start.py
+++ bot/start.py
@@ -5,6 +5,9 @@ from logging import getLogger
 from telegram import Update
 from telegram.ext import CallbackContext, CommandHandler, Dispatcher
 
+from core.keyboards import main_keyboard
+from core.constants import CHOOSING
+
 # Init logger
 logger = getLogger(__name__)
 
@@ -16,4 +19,8 @@ def init(dispatcher: Dispatcher):
 
 def start(update: Update, _: CallbackContext):
     """Process a /start command."""
-    update.message.reply_text(text="Hi")
+    welcome_message: str = """
+    Hi, Welcome To Instagram in Telegram Bot, With this robot you can do whatever you do on Instagram using the telegram with additional features.
+    """
+    update.message.reply_text(text=welcome_message, reply_markup=main_keyboard)
+    return CHOOSING
