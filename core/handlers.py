0ba9f901a92e8e0e7c1599c73bbb41027a999b6b
Reza Shakeri <rzashakeri@gmail.com>
2023-03-15 21:24:55 +0330

    ✨ feat: added HOME and LOGIN_TO_INSTAGRAM state

--- core/handlers.py
+++ core/handlers.py
@@ -2,12 +2,15 @@ from logging import getLogger
 
 from telegram.ext import (
     CommandHandler,
-    ConversationHandler,
+    ConversationHandler, MessageHandler, filters,
 )
 
+from commands.login import get_login_data, login
 from commands.start import start
 
 # Init logger
+from core.constants import HOME, LOGIN, LOGIN_TO_INSTAGRAM
+
 logger = getLogger(__name__)
 
 
@@ -15,8 +18,14 @@ def base_conversation_handler():
     """Process a /start command."""
     conversation_handler = ConversationHandler(
         entry_points=[CommandHandler("start", start)],
-        states={},
+        states={
+            HOME: [
+                MessageHandler(filters.Regex(f"^{LOGIN}$"), get_login_data)
+            ],
+            LOGIN_TO_INSTAGRAM: [
+                MessageHandler(filters.TEXT, login)
+            ]
+        },
         fallbacks=[],
-        map_to_parent={},
     )
     return conversation_handler
