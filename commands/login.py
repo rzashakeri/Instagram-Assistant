d7aead5098b74b41e3d3359875c0f5a505bb71c9
Reza Shakeri <rzashakeri@gmail.com>
2023-03-16 22:00:14 +0330

    ✨ feat: added login.py

---
+++ commands/login.py
@@ -0,0 +1,48 @@
+# encoding: utf-8
+
+from logging import getLogger
+
+from instagrapi import Client
+from telegram import Update
+from telegram.ext import ContextTypes
+
+from core.constants import HOME, BACK_TO_HOME, BACK, LOGIN, LOGIN_TO_INSTAGRAM
+from core.keyboards import base_keyboard, back_keyboard
+
+# Init logger
+logger = getLogger(__name__)
+
+
+async def get_login_data(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
+    """Select an action: Adding parent/child or show data."""
+
+    message_for_get_login_data: str = (
+        "Please send me login information in the following format and format: \n"
+        "\n"
+        "username\n"
+        "password"
+    )
+    await update.message.reply_text(
+        message_for_get_login_data, reply_markup=back_keyboard
+    )
+    return LOGIN_TO_INSTAGRAM
+
+
+async def login(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
+    """Select an action: Adding parent/child or show data."""
+    message = update.message.text
+    if message == BACK:
+        await update.message.reply_text(
+            "what do you want ?", reply_markup=base_keyboard
+        )
+        return HOME
+    username, password = message.split('\n')
+    client = Client()
+    client.login(username, password)
+    client.dump_settings('/tmp/dump.json')
+    await update.effective_user.send_message('login success')
+
+    await update.message.reply_text(
+        'message', reply_markup=base_keyboard
+    )
+    return HOME
