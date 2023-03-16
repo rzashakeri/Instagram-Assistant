135fc676cc83e1a506e0385e925072d347df3361
Reza Shakeri <rzashakeri@gmail.com>
2023-03-16 22:01:12 +0330

    ♻️ refactor: changed name keyboards

--- core/keyboards.py
+++ core/keyboards.py
@@ -1,13 +1,13 @@
 from telegram import ReplyKeyboardMarkup
 
-from core.constants import DOWNLOAD_MEDIA, UPLOAD_MEDIA, INSIGHT, LOGIN, BACK
+from core.constants import DOWNLOAD_MEDIA, UPLOAD_MEDIA, INSIGHT, LOGIN, BACK_TO_HOME, BACK, DOWNLOAD, UPLOAD
 
 base_reply_keyboard: list = [
-    [DOWNLOAD_MEDIA, UPLOAD_MEDIA],
+    [DOWNLOAD, UPLOAD],
     [INSIGHT],
     [LOGIN],
 ]
-base_keyboard = ReplyKeyboardMarkup(base_reply_keyboard)
+base_keyboard = ReplyKeyboardMarkup(base_reply_keyboard, resize_keyboard=True)
 
-back_reply_keyboard: list = [[BACK]]
-back_keyboard = ReplyKeyboardMarkup(back_reply_keyboard)
+back_reply_keyboard = [[BACK]]
+back_keyboard = ReplyKeyboardMarkup(back_reply_keyboard, resize_keyboard=True)
