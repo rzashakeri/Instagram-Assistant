0632ede659c994d6c3ae438a7177cac93b2e5ba2
Reza Shakeri <rzashakeri@gmail.com>
2023-03-20 13:30:34 +0330

    ✨ feat: added messages constants

---
+++ constants/messages.py
@@ -0,0 +1,19 @@
+MESSAGE_FOR_GET_LOGIN_DATA: str = (
+    "Please send me login information in the following format and format: \n"
+    "\n"
+    "username\n"
+    "password"
+)
+WHAT_DO_YOU_WANT: str = "what do you want ?"
+YOU_WERE_ALREADY_LOGGED_IN: str = "You Were Already Logged In"
+LOGGED_IN_SUCCESSFULLY: str = "Logged In Successfully"
+DOWNLOAD_COMPLETED: str = "Download Complete"
+LINK_IS_INVALID: str = "Link is Invalid, check your Link and Try Again"
+STARTING_DOWNLOAD: str = "Starting Download ..."
+UPLOAD_IN_TELEGRAM: str = "Upload In Telegram ..."
+IS_VIDEO: str = "video"
+WELCOME_MESSAGE: str = (
+    "Hi {first_name}, Welcome To Instagram in Telegram Bot, "
+    "With this robot you can do whatever you do "
+    "on Instagram using the telegram with additional features."
+)
