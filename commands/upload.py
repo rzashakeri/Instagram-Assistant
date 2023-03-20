7866ca41e594f3ce2f044e0ccc6104be78553802
Reza Shakeri <rzashakeri@gmail.com>
2023-03-20 15:01:05 +0330

    ✨ feat: added upload module for upload post in instagram

---
+++ commands/upload.py
@@ -0,0 +1,76 @@
+# encoding: utf-8
+import os
+from logging import getLogger
+
+from instagrapi import Client
+from telegram import Update
+from telegram.ext import ContextTypes
+
+from constants import BACK, LOGIN
+from constants.messages import MESSAGE_FOR_GET_LOGIN_DATA, WHAT_DO_YOU_WANT, YOU_WERE_ALREADY_LOGGED_IN, LOGGED_IN_SUCCESSFULLY, SEND_ME_THE_FILE_YOU_WANT_TO_UPLOAD_ON_INSTAGRAM, SEND_ME_THE_CAPTION_OF_POST_YOU_WANT_TO_UPLOAD_ON_INSTAGRAM
+from constants.states import LOGIN_STATE, HOME_STATE, UPLOAD_STATE, LOGIN_TO_INSTAGRAM_FOR_UPLOAD_MEDIA_STATE, GET_FILE_FOR_UPLOAD_IN_INSTAGRAM_STATE, GET_CAPTION_OF_POST_FOR_UPLOAD_IN_INSTAGRAM_STATE
+from core.keyboards import base_keyboard, back_keyboard
+
+FILE = None
+CAPTION = None
+
+async def get_login_data_for_upload_media(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
+    """Select an action: Adding parent/child or show data."""
+
+    message_for_get_login_data: str = MESSAGE_FOR_GET_LOGIN_DATA
+    await update.message.reply_text(
+        message_for_get_login_data, reply_markup=back_keyboard
+    )
+    return LOGIN_TO_INSTAGRAM_FOR_UPLOAD_MEDIA_STATE
+
+
+async def login_to_instagram_for_upload_media(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
+    """Select an action: Adding parent/child or show data."""
+    message = update.message.text
+    if message == BACK:
+        await update.message.reply_text(WHAT_DO_YOU_WANT, reply_markup=base_keyboard)
+        return HOME_STATE
+    user_id = update.effective_user.id
+    username, password = message.split("\n")
+    current_directory = os.getcwd()
+    login_directory = f"{current_directory}/{LOGIN.lower()}"
+    user_instagram_session = f"{login_directory}/{username}_{user_id}.json"
+    login_directory_is_exist = os.path.isdir(login_directory)
+    user_instagram_session_is_exist = os.path.exists(user_instagram_session)
+    if not login_directory_is_exist:
+        os.makedirs(login_directory)
+    client = Client()
+    if user_instagram_session_is_exist:
+        client.load_settings(user_instagram_session)
+        client.login(username, password)
+        client.get_timeline_feed()
+        await update.effective_user.send_message(
+            SEND_ME_THE_FILE_YOU_WANT_TO_UPLOAD_ON_INSTAGRAM, reply_markup=back_keyboard
+        )
+        return GET_FILE_FOR_UPLOAD_IN_INSTAGRAM_STATE
+    client.login(username, password)
+    client.dump_settings(f"{login_directory}/{username}_{user_id}.json")
+    await update.effective_user.send_message(
+        SEND_ME_THE_FILE_YOU_WANT_TO_UPLOAD_ON_INSTAGRAM, reply_markup=back_keyboard
+    )
+    return GET_FILE_FOR_UPLOAD_IN_INSTAGRAM_STATE
+
+
+async def get_file_for_upload_in_instagram(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
+    """Select an action: Adding parent/child or show data."""
+    global FILE
+    FILE = await context.bot.get_file(update.message.document)
+    await update.effective_user.send_message(
+        SEND_ME_THE_CAPTION_OF_POST_YOU_WANT_TO_UPLOAD_ON_INSTAGRAM, reply_markup=back_keyboard
+    )
+    return GET_CAPTION_OF_POST_FOR_UPLOAD_IN_INSTAGRAM_STATE
+
+
+async def get_caption_of_post_for_upload_in_instagram(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
+    """Select an action: Adding parent/child or show data."""
+    global CAPTION
+    CAPTION = message = update.message.text
+    await update.effective_user.send_message(
+        SEND_ME_THE_CAPTION_OF_POST_YOU_WANT_TO_UPLOAD_ON_INSTAGRAM, reply_markup=back_keyboard
+    )
+    return GET_CAPTION_OF_POST_FOR_UPLOAD_IN_INSTAGRAM_STATE
--- core/handlers.py
+++ core/handlers.py
@@ -10,9 +10,10 @@ from commands.login import get_login_data, login
 from commands.start import start
 
 # Init logger
+from commands.upload import get_login_data_for_upload_media, login_to_instagram_for_upload_media, get_file_for_upload_in_instagram
 
-from constants.keyboards import LOGIN_KEY, DOWNLOAD_KEY
-from constants.states import HOME_STATE, LOGIN_STATE, DOWNLOAD_STATE
+from constants.keyboards import LOGIN_KEY, DOWNLOAD_KEY, UPLOAD_KEY
+from constants.states import HOME_STATE, LOGIN_STATE, DOWNLOAD_STATE, UPLOAD_STATE, LOGIN_TO_INSTAGRAM_FOR_UPLOAD_MEDIA_STATE, GET_FILE_FOR_UPLOAD_IN_INSTAGRAM_STATE, GET_CAPTION_OF_POST_FOR_UPLOAD_IN_INSTAGRAM_STATE
 
 logger = getLogger(__name__)
 
@@ -24,13 +25,24 @@ def base_conversation_handler():
         states={
             HOME_STATE: [
                 MessageHandler(filters.Regex(f"^{LOGIN_KEY}$"), get_login_data),
-                MessageHandler(filters.Regex(f"^{DOWNLOAD_KEY}$"), get_media_link)
+                MessageHandler(filters.Regex(f"^{DOWNLOAD_KEY}$"), get_media_link),
+                MessageHandler(filters.Regex(f"^{UPLOAD_KEY}$"), get_login_data_for_upload_media),
             ],
             LOGIN_STATE: [
                 MessageHandler(filters.TEXT, login)
             ],
             DOWNLOAD_STATE: [
                 MessageHandler(filters.TEXT, download)
+            ],
+            # upload operation
+            LOGIN_TO_INSTAGRAM_FOR_UPLOAD_MEDIA_STATE: [
+                MessageHandler(filters.TEXT, login_to_instagram_for_upload_media)
+            ],
+            GET_FILE_FOR_UPLOAD_IN_INSTAGRAM_STATE: [
+                MessageHandler(filters.TEXT, get_file_for_upload_in_instagram)
+            ],
+            GET_CAPTION_OF_POST_FOR_UPLOAD_IN_INSTAGRAM_STATE: [
+                MessageHandler(filters.TEXT, get_file_for_upload_in_instagram)
             ]
         },
         fallbacks=[],
