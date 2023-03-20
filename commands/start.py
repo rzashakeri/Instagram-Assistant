2078503e2b81e473b8fc00ae6dd0f88df6e67d32
Reza Shakeri <rzashakeri@gmail.com>
2023-03-20 13:06:58 +0330

    ♻️ refactor: added WELCOME_MESSAGE to start command

--- commands/start.py
+++ commands/start.py
@@ -5,7 +5,7 @@ from logging import getLogger
 from telegram import Update
 from telegram.ext import ContextTypes
 
-from core.constants import HOME
+from core.constants import HOME, WELCOME_MESSAGE
 from core.keyboards import base_keyboard
 
 # Init logger
@@ -15,12 +15,8 @@ logger = getLogger(__name__)
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
     """Select an action: Adding parent/child or show data."""
 
-    welcome_message: str = (
-        f"Hi {update.effective_user.first_name}, Welcome To Instagram in Telegram Bot, "
-        "With this robot you can do whatever you do "
-        "on Instagram using the telegram with additional features."
-    )
     await update.message.reply_text(
-        welcome_message, reply_markup=base_keyboard
+        WELCOME_MESSAGE.format(first_name=update.effective_user.first_name),
+        reply_markup=base_keyboard,
     )
     return HOME
