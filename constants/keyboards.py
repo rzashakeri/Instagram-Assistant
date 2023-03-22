2cba7bdba39d0326959142b75d536e5c5c46ab37
Reza Shakeri <rzashakeri@gmail.com>
2023-03-22 17:13:16 +0430

    ♻️ refactor: removed download keyboards constant values

--- constants/keyboards.py
+++ constants/keyboards.py
@@ -4,11 +4,3 @@ UPLOAD_KEY: str = "Upload Media"
 LOGIN_KEY: str = "Login To Instagram"
 INSIGHT_KEY: str = "Insights"
 BACK_KEY: str = "Back"
-
-# download keyboards
-DOWNLOAD_PROFILE_KEY: str = "Download Profile"
-DOWNLOAD_PHOTO_KEY: str = "Download Photo"
-DOWNLOAD_VIDEO_KEY: str = "Download Video"
-DOWNLOAD_REELS_KEY: str = "Download Reels"
-DOWNLOAD_ALBUM_KEY: str = "Download Album"
-DOWNLOAD_IGTV_KEY: str = "Download IGtv"
