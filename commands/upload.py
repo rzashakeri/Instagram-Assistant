21c7e06963ca94cb15802a736de1d19c5cbdad7c
Reza Shakeri <rzashakeri@gmail.com>
2023-03-25 16:13:13 +0430

    ♻️ refactor: send file path

--- commands/upload.py
+++ commands/upload.py
@@ -78,9 +78,9 @@ async def get_file_for_upload_in_instagram(update: Update, context: ContextTypes
         await update.message.reply_text(WHAT_DO_YOU_WANT, reply_markup=base_keyboard)
         return HOME_STATE
     global file
-    file_url = await context.bot.get_file(update.message.document.file_id).get_file_url()
+    file = await update.message.document.get_file()
     await update.effective_user.send_message(
-        file_url, reply_markup=back_keyboard
+        file.file_path, reply_markup=back_keyboard
     )
     return GET_CAPTION_OF_POST_FOR_UPLOAD_IN_INSTAGRAM_STATE
 
