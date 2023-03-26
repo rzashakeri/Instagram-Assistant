40c92857bf365355f064f1895ab4259e99e4b485
Reza Shakeri <rzashakeri@gmail.com>
2023-03-27 01:52:04 +0430

    🐛 fix: fix "IsADirectoryError: [Errno 21] Is a directory: '/home/xxxxxxxx/repositories/instagram-telegram-bot/download'" error

--- commands/upload.py
+++ commands/upload.py
@@ -178,20 +178,16 @@ async def set_media_and_get_caption(
     if message.text == BACK:
         await update.message.reply_text(WHAT_DO_YOU_WANT, reply_markup=base_keyboard)
         return HOME_STATE
-    file_name = update.message.document.file_name
-    media = await update.message.document.get_file()
     global FILE_PATH_ON_SERVER
     current_directory = os.getcwd()
     download_directory = f"{current_directory}/download"
     download_directory_is_exist = os.path.exists(download_directory)
     if not download_directory_is_exist:
         os.makedirs(download_directory)
-    FILE_PATH_ON_SERVER = await media.download_to_drive(
-        custom_path=Path(f"{download_directory}/{file_name}")
-    )
-
+    telegram_file = context.bot.getFile(update.message.document.file_id)
+    file_path = telegram_file.download(f"{download_directory}/{update.message.document.file_name}")
     await update.effective_user.send_message(
-        FILE_PATH_ON_SERVER,
+        file_path,
         reply_markup=back_keyboard,
     )
     await update.effective_user.send_message(
