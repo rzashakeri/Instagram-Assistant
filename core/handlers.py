8c04f334c6e5b0e015ddee94b339025da7aa3870
Reza Shakeri <rzashakeri@gmail.com>
2023-03-26 21:37:25 +0430

    ✨ feat: added GET_MEDIA_TYPE_STATE SET_MEDIA_TYPE_STATE to states of handlers.py

--- core/handlers.py
+++ core/handlers.py
@@ -13,6 +13,7 @@ from commands.start import start
 
 # Init logger
 from commands import upload
+from commands.upload import get_media_type, set_media_type
 from constants.keyboards import LOGIN_KEY, DOWNLOAD_KEY, UPLOAD_KEY
 from constants.states import (
     HOME_STATE,
@@ -21,7 +22,7 @@ from constants.states import (
     UPLOAD_STATE,
     LOGIN_TO_INSTAGRAM_FOR_UPLOAD_MEDIA_STATE,
     GET_FILE_FOR_UPLOAD_IN_INSTAGRAM_STATE,
-    GET_CAPTION_OF_POST_FOR_UPLOAD_IN_INSTAGRAM_STATE,
+    GET_CAPTION_OF_POST_FOR_UPLOAD_IN_INSTAGRAM_STATE, GET_MEDIA_TYPE_STATE, SET_MEDIA_TYPE_STATE,
 )
 
 logger = getLogger(__name__)
@@ -44,6 +45,12 @@ def base_conversation_handler():
             # upload operation
             LOGIN_TO_INSTAGRAM_FOR_UPLOAD_MEDIA_STATE: [
                 MessageHandler(filters.TEXT, upload.login)
+            ],
+            GET_MEDIA_TYPE_STATE: [
+                MessageHandler(filters.TEXT, get_media_type)
+            ],
+            SET_MEDIA_TYPE_STATE: [
+                MessageHandler(filters.TEXT, set_media_type)
             ],
             GET_FILE_FOR_UPLOAD_IN_INSTAGRAM_STATE: [
                 MessageHandler(
