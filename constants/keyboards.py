4045baa7ded64865be49e35e820bbc8c6da0afdf
Reza Shakeri <rzashakeri@gmail.com>
2023-03-26 20:51:22 +0430

    ✨ feat: added upload the media type keys

--- constants/keyboards.py
+++ constants/keyboards.py
@@ -4,3 +4,10 @@ UPLOAD_KEY: str = "Upload Media"
 LOGIN_KEY: str = "Login To Instagram"
 INSIGHT_KEY: str = "Insights"
 BACK_KEY: str = "Back"
+
+# upload the media type keys
+UPLOAD_REELS_KEY: str = "Upload Reels"
+UPLOAD_PHOTO_KEY: str = "Upload Photo"
+UPLOAD_VIDEO_KEY: str = "Upload IGtv"
+UPLOAD_ALBUM_KEY: str = "Upload Album"
+UPLOAD_IGTV_KEY: str = "Upload IGtv"
