c254c44aecfde7ffaff24528da36d61ad25d616a
Reza Shakeri <rzashakeri@gmail.com>
2023-03-29 13:16:13 +0430

    ✨ feat: added challenge_resolve

--- commands/upload.py
+++ commands/upload.py
@@ -122,14 +122,7 @@ async def login_attempt_and_get_media_type(
                 return HOME_STATE
     CLIENT.login(username, password)
     CLIENT.dump_settings(f"{login_directory}/{username}_{user_id}.json")
-    instagram_api_response = CLIENT.last_json
-    challenge_type = instagram_api_response.get("challenge_type_enum_str")
-    if challenge_type == "HACKED_LOCK":
-        await update.effective_user.send_message(
-            "challenge required, Please try again later",
-            reply_markup=base_keyboard,
-        )
-        return HOME_STATE
+    CLIENT.challenge_resolve(CLIENT.last_json)
     await update.effective_user.send_message(
         WHAT_TYPE_OF_CONTENT_DO_YOU_WANT_TO_UPLOAD_ON_INSTAGRAM,
         reply_markup=media_type_keyboard,
