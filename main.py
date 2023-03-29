9fbeeb6db48c2d654bed29ad0360dd0699c21586
Reza Shakeri <rzashakeri@gmail.com>
2023-03-29 17:13:13 +0430

    ✨ feat: added read_timeout and get_updates_read_timeout

--- main.py
+++ main.py
@@ -5,7 +5,13 @@ from core.handlers import base_conversation_handler
 
 if __name__ == "__main__":
     logger.init_logger(f"logs/{settings.NAME}.log")
-    application = Application.builder().token(settings.TOKEN).build()
+    application = (
+        Application.builder()
+        .token(settings.TOKEN)
+        .read_timeout(7)
+        .get_updates_read_timeout(42)
+        .build()
+    )
     conversation_handler = base_conversation_handler()
     application.add_handler(conversation_handler)
     application.run_polling()
