446b0502b9e9d54670732e857f81a4143eb8128b
Reza Shakeri <rzashakeri@gmail.com>
2023-03-29 14:04:52 +0430

    ✨ feat: added email_or_phone_keyboard

--- core/keyboards.py
+++ core/keyboards.py
@@ -1,6 +1,6 @@
 from telegram import ReplyKeyboardMarkup
 
-from constants import YES, NO
+from constants import YES, NO, EMAIL, PHONE
 from constants.keyboards import DOWNLOAD_KEY, UPLOAD_KEY, INSIGHT_KEY, LOGIN_KEY, BACK_KEY, UPLOAD_REELS_KEY, UPLOAD_PHOTO_KEY, UPLOAD_VIDEO_KEY, UPLOAD_ALBUM_KEY, UPLOAD_IGTV_KEY
 
 base_reply_keyboard: list = [
@@ -26,3 +26,10 @@ yes_or_no_reply_keyboard = [
     [NO], [YES]
 ]
 yes_or_no_keyboard = ReplyKeyboardMarkup(yes_or_no_reply_keyboard, resize_keyboard=True)
+
+
+email_or_phone_reply_keyboard = [
+    [EMAIL],
+    [PHONE]
+]
+email_or_phone_keyboard = ReplyKeyboardMarkup(email_or_phone_reply_keyboard, resize_keyboard=True)
