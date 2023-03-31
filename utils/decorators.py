10ada8d499dbdeec5282cb41b293884c6a33ec1e
Reza Shakeri <rzashakeri@gmail.com>
2023-03-31 13:53:03 +0430

    ✨ feat: added restricted and send_action function

---
+++ utils/decorators.py
@@ -0,0 +1,32 @@
+from functools import wraps
+
+from configurations.settings import LIST_OF_ADMINS
+
+
+def restricted(func):
+    """This decorator allows you to restrict the access of a handler to
+    only the user_ids specified in LIST_OF_ADMINS."""
+
+    @wraps(func)
+    async def wrapped(update, context, *args, **kwargs):
+        user_id = update.effective_user.id
+        if user_id not in LIST_OF_ADMINS:
+            print(f"Unauthorized access denied for {user_id}.")
+            return
+        return await func(update, context, *args, **kwargs)
+
+    return wrapped
+
+
+def send_action(action):
+    """Sends `action` while processing func command."""
+
+    def decorator(func):
+        @wraps(func)
+        async def command_func(update, context, *args, **kwargs):
+            await context.bot.send_chat_action(chat_id=update.effective_message.chat_id, action=action)
+            return await func(update, context, *args, **kwargs)
+
+        return command_func
+
+    return decorator
