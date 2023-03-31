b6070b00542e43163824d1bdd87681c67f9469a5
Reza Shakeri <rzashakeri@gmail.com>
2023-03-31 13:30:37 +0430

    ✨ feat: added @restricted to admin.py

--- commands/admin.py
+++ commands/admin.py
@@ -10,15 +10,18 @@ from constants.messages import WELCOME_TO_ADMIN
 from constants.states import HOME_STATE
 from core.keyboards import base_keyboard
 
+from utils import restricted
+
 # Init logger
 logger = getLogger(__name__)
 
 
+@restricted
 async def admin(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
     """Select an action: Adding parent/child or show data."""
     # pylint: disable=unused-argument
     await update.message.reply_text(
-            WELCOME_TO_ADMIN,
-            reply_markup=base_keyboard,
+        WELCOME_TO_ADMIN,
+        reply_markup=base_keyboard,
     )
     return HOME_STATE
