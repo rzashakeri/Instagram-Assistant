667ddee8424213be29d5c6cf99cacd8ca289e5f7
Reza Shakeri <rzashakeri@gmail.com>
2023-03-31 14:08:16 +0430

    ♻️ refactor: added is typing ... action to start message

--- commands/start.py
+++ commands/start.py
@@ -3,6 +3,7 @@
 from logging import getLogger
 
 from telegram import Update
+from telegram.constants import ChatAction
 from telegram.ext import ContextTypes
 
 from constants.messages import WELCOME_MESSAGE
@@ -10,9 +11,12 @@ from constants.states import HOME_STATE
 from core.keyboards import base_keyboard
 
 # Init logger
+from utils.decorators import send_action
+
 logger = getLogger(__name__)
 
 
+@send_action(ChatAction.TYPING)
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
     """Select an action: Adding parent/child or show data."""
     # pylint: disable=unused-argument
