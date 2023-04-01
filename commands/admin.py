f6a22d404df5d4b7064ec0ae20faa3de6de682ba
Reza Shakeri <rzashakeri@gmail.com>
2023-04-01 23:27:42 +0430

    ♻️ refactor: added is typing ... action to admin command

--- commands/admin.py
+++ commands/admin.py
@@ -3,19 +3,21 @@
 from logging import getLogger
 
 from telegram import Update
+from telegram.constants import ChatAction
 from telegram.ext import ContextTypes
 
 from constants.messages import WELCOME_TO_ADMIN
 from constants.states import ADMIN_STATE
 from core.keyboards import base_keyboard, admin_keyboard
 
-from utils.decorators import restricted
+from utils.decorators import restricted, send_action
 
 # Init logger
 logger = getLogger(__name__)
 
 
 @restricted
+@send_action(ChatAction.TYPING)
 async def admin(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
     """Select an action: Adding parent/child or show data."""
     # pylint: disable=unused-argument
