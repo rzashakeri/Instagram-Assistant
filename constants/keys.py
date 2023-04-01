1d3ea09b290c191868b334c60ece4020b3df988b
Reza Shakeri <rzashakeri@gmail.com>
2023-04-01 23:37:22 +0430

    ♻️ refactor: added emoji to media type keyboard

--- constants/keys.py
+++ constants/keys.py
@@ -6,12 +6,12 @@ INSIGHT_KEY: str = "Insights 📈"
 BACK_KEY: str = "Back ◀️"
 
 # upload the media type keys
-UPLOAD_REELS_KEY: str = "Upload Reels"
-UPLOAD_PHOTO_KEY: str = "Upload Photo"
-UPLOAD_VIDEO_KEY: str = "Upload IGtv"
-UPLOAD_ALBUM_KEY: str = "Upload Album"
-UPLOAD_IGTV_KEY: str = "Upload IGtv"
+UPLOAD_REELS_KEY: str = "Upload Reels 📤"
+UPLOAD_PHOTO_KEY: str = "Upload Photo 📤"
+UPLOAD_VIDEO_KEY: str = "Upload IGtv 📤"
+UPLOAD_ALBUM_KEY: str = "Upload Album 📤"
+UPLOAD_IGTV_KEY: str = "Upload IGtv 📤"
 
 # admin panel keys
-BACK_TO_HOME_KEY: str = "Back To Home"
+BACK_TO_HOME_KEY: str = "Back To Home 🏚️"
 SEND_MESSAGE_TO_ALL_USER_KEY: str = "Send Message To All Users"
