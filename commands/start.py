d9f00f3740d56e54cb90b7941ac176a95d6adb70
Reza Shakeri <rzashakeri@gmail.com>
2023-04-05 14:39:10 +0430

    ✨ feat: added create_user function when user send /start command and saved user information

--- commands/start.py
+++ commands/start.py
@@ -2,11 +2,12 @@
 import time
 import psycopg2
 from logging import getLogger
-
+from datetime import datetime, timezone
 from telegram import Update
 from telegram.constants import ChatAction
 from telegram.ext import ContextTypes
 
+from connectors.postgresql import create_user
 from constants.messages import WELCOME_MESSAGE
 from constants.states import HOME_STATE
 from core.keyboards import base_keyboard
@@ -21,9 +22,14 @@ logger = getLogger(__name__)
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
     """Select an action: Adding parent/child or show data."""
     # pylint: disable=unused-argument
+    user_id = update.effective_user.id
+    first_name = update.effective_user.first_name
+    last_name = update.effective_user.last_name
+    username = update.effective_user.username
+    create_user(user_id, first_name, last_name, username)
     time.sleep(5)
     await update.message.reply_text(
-        WELCOME_MESSAGE.format(first_name=update.effective_user.first_name),
+        WELCOME_MESSAGE.format(first_name=first_name),
         reply_markup=base_keyboard,
     )
     return HOME_STATE
