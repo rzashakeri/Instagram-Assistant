771d780afbbda1b0f3ce5492a9766ab0fe4a6816
Reza Shakeri <rzashakeri@gmail.com>
2023-04-07 03:18:24 +0430

    ✨ feat: added PRIVACY_MESSAGE to messages.py

--- constants/messages.py
+++ constants/messages.py
@@ -64,3 +64,8 @@ USER_COUNT: str = "User Count: {user_count}"
 WELCOME_TO_HOME: str = "Welcome To Home, What are you doing?"
 SEND_YOUR_MESSAGE: str = "Send Your Message 👇"
 YOUR_MESSAGE_WAS_SENT: str = "Your Message Was Sent"
+PRIVACY_MESSAGE: str = (
+    "This robot stores the following information:\n"
+    "User ID,First Name, Last Name, Username\n"
+    "And when you Login Instagram via this robot, we store the session inside our servers"
+)
