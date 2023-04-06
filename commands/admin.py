ecb669682b581c176f5fc51e38a19e380de63ced
Reza Shakeri <rzashakeri@gmail.com>
2023-04-06 05:10:15 +0430

    ✨ feat: added get_message_for_send_to_all_user and send_message_to_all_user command

--- commands/admin.py
+++ commands/admin.py
@@ -6,10 +6,10 @@ from telegram import Update
 from telegram.constants import ChatAction
 from telegram.ext import ContextTypes
 
-from connectors.postgresql import get_user_count
+from connectors.postgresql import get_user_count, get_user_id
 from constants.keys import BACK_TO_HOME_KEY
-from constants.messages import WELCOME_TO_ADMIN, USER_COUNT, WELCOME_TO_HOME
-from constants.states import ADMIN_STATE, HOME_STATE
+from constants.messages import WELCOME_TO_ADMIN, USER_COUNT, WELCOME_TO_HOME, SEND_YOUR_MESSAGE, YOUR_MESSAGE_WAS_SENT
+from constants.states import ADMIN_STATE, HOME_STATE, SEND_MESSAGE_TO_ALL_USER
 from core.keyboards import base_keyboard, admin_keyboard
 
 from utils.decorators import restricted, send_action
@@ -54,3 +54,32 @@ async def back_to_home(update: Update, context: ContextTypes.DEFAULT_TYPE) -> st
         reply_markup=base_keyboard,
     )
     return HOME_STATE
+
+
+@restricted
+@send_action(ChatAction.TYPING)
+async def get_message_for_send_to_all_user(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
+    """get user count"""
+    # pylint: disable=unused-argument
+    await update.message.reply_text(
+        SEND_YOUR_MESSAGE,
+        reply_markup=base_keyboard,
+    )
+    return SEND_MESSAGE_TO_ALL_USER
+
+
+@restricted
+@send_action(ChatAction.TYPING)
+async def send_message_to_all_user(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
+    """get user count"""
+    # pylint: disable=unused-argument
+    message = update.message.text
+    columns = get_user_id()
+    for row in columns:
+        for user_id in row:
+            await context.bot.send_message(chat_id=user_id, text=message)
+    await update.message.reply_text(
+        YOUR_MESSAGE_WAS_SENT,
+        reply_markup=base_keyboard,
+    )
+    return ADMIN_STATE
