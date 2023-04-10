198768cf0aedb05dbf7dd28ce6334b131ce999cb
Restyled.io <commits@restyled.io>
2023-04-10 10:08:57 +0000

    Restyled by autopep8

--- commands/login.py
+++ commands/login.py
@@ -65,7 +65,8 @@ async def login(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
         except LoginRequired:
             os.remove(user_instagram_session)
             client.login(username, password)
-            client.dump_settings(f"{login_directory}/{username}_{user_id}.json")
+            client.dump_settings(
+                f"{login_directory}/{username}_{user_id}.json")
         except ClientForbiddenError:
             await update.effective_user.send_message(
                 SOMETHING_WENT_WRONG,
