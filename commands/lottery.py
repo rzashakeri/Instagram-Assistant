b69c3944cfb3fbc41b080eb254d75e1a08488337
Restyled.io <commits@restyled.io>
2023-04-10 10:41:31 +0000

    Restyled by isort

--- commands/lottery.py
+++ commands/lottery.py
@@ -1,15 +1,13 @@
 # encoding: utf-8
 from logging import getLogger
+
 from telegram import Update
 from telegram.constants import ChatAction
 from telegram.ext import ContextTypes
 
 from connectors.postgresql import create_user
-from constants.messages import (
-    WELCOME_MESSAGE,
-    PRIVACY_MESSAGE,
-    WELCOME_TO_THE_LOTTERY_SECTION,
-)
+from constants.messages import (PRIVACY_MESSAGE, WELCOME_MESSAGE,
+                                WELCOME_TO_THE_LOTTERY_SECTION)
 from constants.states import HOME_STATE
 from core.keyboards import base_keyboard, lottery_keyboard
 from utils.decorators import send_action
