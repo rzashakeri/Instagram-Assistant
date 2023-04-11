762d8f0529029e58cb3abcc2df6fc6742db0eb66
Reza Shakeri <rzashakeri@gmail.com>
2023-04-12 02:12:17 +0430

    ✨ feat: implementation lottery with like

--- commands/lottery.py
+++ commands/lottery.py
@@ -1,11 +1,16 @@
 # encoding: utf-8
+import random
+import time
 from logging import getLogger
 
 import validators
+from instagrapi import Client
+from instagrapi.exceptions import MediaNotFound
 from telegram import Update
 from telegram.constants import ChatAction
 from telegram.ext import ContextTypes
 
+from commands.login import login_user
 from connectors.postgresql import create_user
 from constants.keys import BACK_KEY
 from constants.messages import (
@@ -28,6 +33,8 @@ from utils.decorators import send_action
 logger = getLogger(__name__)
 
 POST_LINK = None
+CLIENT = Client()
+CLIENT.delay_range = [1, 3]
 
 
 @send_action(ChatAction.TYPING)
@@ -74,6 +81,80 @@ async def lottery_with_likes_list(
     if message == BACK_KEY:
         await update.message.reply_text(WHAT_DO_YOU_WANT, reply_markup=base_keyboard)
         return HOME_STATE
+    try:
+        login_user(CLIENT)
+
+        # start getting post information ==>
+        processing_message = await context.bot.send_message(
+            chat_id=update.message.chat_id, text="Processing ..."
+        )
+        media_pk_from_url = CLIENT.media_pk_from_url(POST_LINK)
+        media_id = CLIENT.media_id(media_pk_from_url)
+        await context.bot.deleteMessage(
+            message_id=processing_message.message_id, chat_id=update.message.chat_id
+        )
+
+        # getting like the list from instagram ==>
+        getting_likes_list_message = await context.bot.send_message(
+            chat_id=update.message.chat_id, text="Grabbing Likes List ..."
+        )
+        media_likers = CLIENT.media_likers(media_id)
+        await context.bot.deleteMessage(
+            message_id=getting_likes_list_message.message_id,
+            chat_id=update.message.chat_id,
+        )
+
+        # find winner ==>
+        find_winner_message = await context.bot.send_message(
+            chat_id=update.message.chat_id, text="Finding Winner ... 🎖️"
+        )
+        winner = random.choice(media_likers)
+        await context.bot.deleteMessage(
+            message_id=find_winner_message.message_id, chat_id=update.message.chat_id
+        )
+
+        countdown_message = await context.bot.send_message(
+            chat_id=update.message.chat_id,
+            text="Countdown to introduce the winner 🕐",
+        )
+        time.sleep(1)
+        await context.bot.deleteMessage(
+            message_id=countdown_message.message_id, chat_id=update.message.chat_id
+        )
+
+        number_three = await context.bot.send_message(
+            chat_id=update.message.chat_id, text="3️⃣"
+        )
+        time.sleep(1)
+        await context.bot.deleteMessage(
+            message_id=number_three.message_id, chat_id=update.message.chat_id
+        )
+
+        number_two = await context.bot.send_message(
+            chat_id=update.message.chat_id, text="2️⃣"
+        )
+        time.sleep(1)
+        await context.bot.deleteMessage(
+            message_id=number_two.message_id, chat_id=update.message.chat_id
+        )
+
+        number_one = await context.bot.send_message(
+            chat_id=update.message.chat_id, text="1️⃣"
+        )
+        time.sleep(1)
+        await context.bot.deleteMessage(
+            message_id=number_one.message_id, chat_id=update.message.chat_id
+        )
+
+        await context.bot.send_message(
+            chat_id=update.message.chat_id,
+            text=f"https://instagram.com/{winner.username}",
+            reply_markup=base_keyboard,
+        )
+    except MediaNotFound:
+        await update.message.reply_text(
+            "media not found, check your link and try again", reply_markup=base_keyboard
+        )
 
 
 @send_action(ChatAction.TYPING)
