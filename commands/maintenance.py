beb6b032734251b14a77a04f50a3f03018a479b3
Reza Shakeri <rzashakeri@gmail.com>
2023-04-24 02:29:27 +0430

    ✨ feat: added maintenance mode

---
+++ commands/maintenance.py
@@ -0,0 +1,21 @@
+# encoding: utf-8
+from logging import getLogger
+
+from telegram import Update
+from telegram.constants import ChatAction
+from telegram.ext import ContextTypes
+from utils.decorators import send_action
+
+# Init logger
+
+logger = getLogger(__name__)
+
+
+@send_action(ChatAction.TYPING)
+async def maintenance(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
+    """Select an action: Adding parent/child or show data."""
+    # pylint: disable=unused-argument
+    await context.bot.send_message(
+        chat_id=update.message.chat_id,
+        text="Bot Under Maintenance 🛠️, Thank you for waiting"
+    )
