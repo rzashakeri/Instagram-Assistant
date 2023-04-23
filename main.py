1b0636221f63fa8565540e484c024d76ed367d02
Reza Shakeri <rzashakeri@gmail.com>
2023-04-24 02:29:54 +0430

    ✨ feat: added maintenance mode to main.py

--- main.py
+++ main.py
@@ -4,12 +4,17 @@ from configurations import settings
 from core.handlers import base_conversation_handler
 from utils import create_requirement_folders
 from utils import logger
+from configurations.settings import IS_MAINTENANCE
+from commands.maintenance import maintenance
 
 if __name__ == "__main__":
     logger.init_logger(f"logs/{settings.NAME}.log")
     create_requirement_folders()
     application = (Application.builder().token(settings.TOKEN).read_timeout(
         50).write_timeout(50).get_updates_read_timeout(50).build())
-    conversation_handler = base_conversation_handler()
-    application.add_handler(conversation_handler)
+    if IS_MAINTENANCE:
+        application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, maintenance))
+    else:
+        conversation_handler = base_conversation_handler()
+        application.add_handler(conversation_handler)
     application.run_polling()
