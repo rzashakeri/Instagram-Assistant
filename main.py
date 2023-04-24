00d0d0bfd2ee2ba524bf9fce58d8a4a7439994cb
Restyled.io <commits@restyled.io>
2023-04-24 11:19:34 +0000

    Restyled by yapf

--- main.py
+++ main.py
@@ -12,19 +12,12 @@ from utils import logger
 if __name__ == "__main__":
     logger.init_logger(f"logs/{settings.NAME}.log")
     create_requirement_folders()
-    application = (
-        Application.builder()
-        .token(settings.TOKEN)
-        .read_timeout(50)
-        .write_timeout(50)
-        .get_updates_read_timeout(50)
-        .build()
-    )
+    application = (Application.builder().token(settings.TOKEN).read_timeout(
+        50).write_timeout(50).get_updates_read_timeout(50).build())
     if IS_MAINTENANCE:
         application.add_handler(CommandHandler("start", maintenance))
         application.add_handler(
-            MessageHandler(filters.TEXT & ~filters.COMMAND, maintenance)
-        )
+            MessageHandler(filters.TEXT & ~filters.COMMAND, maintenance))
     else:
         conversation_handler = base_conversation_handler()
         application.add_handler(conversation_handler)
