945266a337ca4af9927345a184251823c7a56b82
Restyled.io <commits@restyled.io>
2023-04-24 11:02:48 +0000

    Restyled by reorder-python-imports

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from commands.maintenance import maintenance
 from configurations import settings
 from configurations.settings import IS_MAINTENANCE
 from core.handlers import base_conversation_handler
-from utils import create_requirement_folders, logger
+from utils import create_requirement_folders
+from utils import logger
 
 if __name__ == "__main__":
     logger.init_logger(f"logs/{settings.NAME}.log")
