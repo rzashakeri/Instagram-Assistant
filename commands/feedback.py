82b49ceb0b6befd856d1f81c121bea4829e14fe7
Reza shakeri <rzashakeri@gmail.com>
2023-05-12 00:13:02 +0330

    ✨ feat: added send_feedback

--- commands/feedback.py
+++ commands/feedback.py
@@ -11,10 +11,11 @@ from telegram.constants import ParseMode
 from telegram.ext import ContextTypes
 from telegram.ext import ConversationHandler
 
+from configurations.settings import ADMIN_TELEGRAM_USER_ID
 from connectors.postgresql import create_user
 from constants import YES, NO
 from constants.keys import BACK_KEY
-from constants.messages import GOODBYE_WE_ARE_SORRY
+from constants.messages import GOODBYE_WE_ARE_SORRY, FEEDBACK_MESSAGE, NEW_MESSAGE, YOUR_MESSAGE_WAS_SENT
 from constants.messages import PRIVACY_MESSAGE
 from constants.messages import WELCOME_MESSAGE
 from constants.states import HOME_STATE
@@ -30,4 +31,26 @@ logger = getLogger(__name__)
 async def get_feedback(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
     """Select an action: Adding parent/child or show data."""
     # pylint: disable=unused-argument
-    pass
+    await context.bot.send_message(
+        chat_id=update.message.chat_id, text=FEEDBACK_MESSAGE)
+
+
+@send_action(ChatAction.TYPING)
+async def send_feedback(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
+    """Select an action: Adding parent/child or show data."""
+    # pylint: disable=unused-argument
+    message = update.message.text
+    user_id = update.effective_user.id
+    first_name = update.effective_user.first_name
+    last_name = update.effective_user.last_name
+    username = update.effective_user.username
+    await context.bot.send_message(
+        chat_id=ADMIN_TELEGRAM_USER_ID,
+        text=NEW_MESSAGE.format(user_id=user_id, first_name=first_name,
+                                last_name=last_name, username=username,
+                                message=message),
+        parse_mode=ParseMode.MARKDOWN
+    )
+    await context.bot.send_message(
+        chat_id=update.message.chat_id,
+        text=YOUR_MESSAGE_WAS_SENT)
