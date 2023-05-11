4ee51ce0d0f03aec9779b77b684445c91d5918aa
Reza shakeri <rzashakeri@gmail.com>
2023-05-12 02:57:16 +0330

    ✨ feat: added share count to insight

--- commands/insight.py
+++ commands/insight.py
@@ -84,7 +84,10 @@ async def insight(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
         comment_count = insight_of_media.get("comment_count")
         like_count = insight_of_media.get("like_count")
         save_count = insight_of_media.get("save_count")
-        share_count = insight_of_media.get("reshare_count")
+        nodes = insight_of_media["inline_insights_node"]["metrics"]["share_count"]["tray"]["nodes"]
+        share_count = 0
+        for node in nodes:
+            share_count += node["value"]
         await context.bot.deleteMessage(
             message_id=bot_message.message_id,
             chat_id=update.message.chat_id,
