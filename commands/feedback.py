fff93aa4271f3533356264cce1df158dbf64b6a8
Restyled.io <commits@restyled.io>
2023-05-11 22:40:53 +0000

    Restyled by black

--- commands/feedback.py
+++ commands/feedback.py
@@ -15,7 +15,14 @@ from configurations.settings import ADMIN_TELEGRAM_USER_ID
 from connectors.postgresql import create_user
 from constants import YES, NO
 from constants.keys import BACK_KEY
-from constants.messages import GOODBYE_WE_ARE_SORRY, FEEDBACK_MESSAGE, NEW_MESSAGE, YOUR_MESSAGE_WAS_SENT, NEW_TEXT_MESSAGE, WHAT_DO_YOU_WANT
+from constants.messages import (
+    GOODBYE_WE_ARE_SORRY,
+    FEEDBACK_MESSAGE,
+    NEW_MESSAGE,
+    YOUR_MESSAGE_WAS_SENT,
+    NEW_TEXT_MESSAGE,
+    WHAT_DO_YOU_WANT,
+)
 from constants.messages import PRIVACY_MESSAGE
 from constants.messages import WELCOME_MESSAGE
 from constants.states import HOME_STATE, FEEDBACK_STATE
@@ -32,7 +39,10 @@ async def get_feedback(update: Update, context: ContextTypes.DEFAULT_TYPE) -> st
     """Select an action: Adding parent/child or show data."""
     # pylint: disable=unused-argument
     await context.bot.send_message(
-        chat_id=update.message.chat_id, text=FEEDBACK_MESSAGE, reply_markup=back_keyboard)
+        chat_id=update.message.chat_id,
+        text=FEEDBACK_MESSAGE,
+        reply_markup=back_keyboard,
+    )
     return FEEDBACK_STATE
 
 
@@ -47,21 +57,22 @@ async def send_feedback(update: Update, context: ContextTypes.DEFAULT_TYPE) -> s
     username = update.effective_user.username
 
     if message == BACK_KEY:
-        await update.message.reply_text(WHAT_DO_YOU_WANT,
-                                        reply_markup=base_keyboard)
+        await update.message.reply_text(WHAT_DO_YOU_WANT, reply_markup=base_keyboard)
         return HOME_STATE
     await context.bot.send_message(
         chat_id=ADMIN_TELEGRAM_USER_ID,
         text=NEW_TEXT_MESSAGE.format(
-            user_id=user_id, first_name=first_name,
-            last_name=last_name, username=username,
-            message=message
+            user_id=user_id,
+            first_name=first_name,
+            last_name=last_name,
+            username=username,
+            message=message,
         ),
-        parse_mode=ParseMode.MARKDOWN_V2
+        parse_mode=ParseMode.MARKDOWN_V2,
     )
     await context.bot.send_message(
         chat_id=update.message.chat_id,
         text=YOUR_MESSAGE_WAS_SENT,
-        reply_markup=base_keyboard
+        reply_markup=base_keyboard,
     )
     return HOME_STATE
