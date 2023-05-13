927b62e2085e9710e51bed0155460d090708e8a7
Restyled.io <commits@restyled.io>
2023-05-13 14:42:06 +0000

    Restyled by yapf

--- commands/download.py
+++ commands/download.py
@@ -53,12 +53,12 @@ logger = getLogger(__name__)
 
 
 @send_action(ChatAction.TYPING)
-async def get_media_link(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
+async def get_media_link(update: Update,
+                         context: ContextTypes.DEFAULT_TYPE) -> str:
     """Select an action: Adding parent/child or show data."""
     # pylint: disable=unused-argument
-    await update.message.reply_text(
-        OK_SEND_ME_THE_LINK_YOU_WANT_TO_DOWNLOAD, reply_markup=back_keyboard
-    )
+    await update.message.reply_text(OK_SEND_ME_THE_LINK_YOU_WANT_TO_DOWNLOAD,
+                                    reply_markup=back_keyboard)
     return DOWNLOAD_STATE
 
 
@@ -68,17 +68,16 @@ async def download(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
     # pylint: disable=unused-argument
     message = update.message.text
     if message == BACK_KEY:
-        await update.message.reply_text(WHAT_DO_YOU_WANT, reply_markup=base_keyboard)
+        await update.message.reply_text(WHAT_DO_YOU_WANT,
+                                        reply_markup=base_keyboard)
         return HOME_STATE
     client = Client()
     client.delay_range = [1, 3]
     message_is_url = validators.url(message)
     await context.bot.send_chat_action(
-        chat_id=update.effective_message.chat_id, action=ChatAction.TYPING
-    )
+        chat_id=update.effective_message.chat_id, action=ChatAction.TYPING)
     bot_message = await context.bot.send_message(
-        chat_id=update.message.chat_id, text=PROCESSING
-    )
+        chat_id=update.message.chat_id, text=PROCESSING)
     logged_in_user = login_admin_user_to_instagram(client)
     if not logged_in_user:
         await context.bot.editMessageText(
@@ -136,7 +135,8 @@ async def download(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
                     regex = r"(?<=instagram.com\/)[A-Za-z0-9_.]+"
                     username = re.findall(regex, message)[0]
                     try:
-                        user_data = client.user_info_by_username(username).dict()
+                        user_data = client.user_info_by_username(
+                            username).dict()
                     except UserNotFound:
                         await context.bot.deleteMessage(
                             message_id=bot_message.message_id,
@@ -153,8 +153,8 @@ async def download(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
                     )
                     user_profile_picture_url = user_data["profile_pic_url_hd"]
                     await update.effective_user.send_photo(
-                        photo=user_profile_picture_url, reply_markup=base_keyboard
-                    )
+                        photo=user_profile_picture_url,
+                        reply_markup=base_keyboard)
                     return HOME_STATE
         if media_type == PHOTO:
             await context.bot.deleteMessage(
@@ -162,8 +162,8 @@ async def download(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
                 chat_id=update.message.chat_id,
             )
             await context.bot.send_chat_action(
-                chat_id=update.effective_message.chat_id, action=ChatAction.UPLOAD_PHOTO
-            )
+                chat_id=update.effective_message.chat_id,
+                action=ChatAction.UPLOAD_PHOTO)
             await update.effective_user.send_photo(
                 photo=media_info["thumbnail_url"],
                 caption=MEDIA_CAPTION.format(
@@ -173,21 +173,16 @@ async def download(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
                 reply_markup=base_keyboard,
             )
             return HOME_STATE
-        elif (
-            media_type == VIDEO
-            and product_type == IS_FEED
-            or media_type == IGTV
-            and product_type == IS_IGTV
-            or media_type == REEL
-            and product_type == IS_CLIPS
-        ):
+        elif (media_type == VIDEO and product_type == IS_FEED
+              or media_type == IGTV and product_type == IS_IGTV
+              or media_type == REEL and product_type == IS_CLIPS):
             await context.bot.deleteMessage(
                 message_id=bot_message.message_id,
                 chat_id=update.message.chat_id,
             )
             await context.bot.send_chat_action(
-                chat_id=update.effective_message.chat_id, action=ChatAction.UPLOAD_VIDEO
-            )
+                chat_id=update.effective_message.chat_id,
+                action=ChatAction.UPLOAD_VIDEO)
             await update.effective_user.send_video(
                 video=media_info["video_url"],
                 caption=MEDIA_CAPTION.format(
@@ -208,13 +203,15 @@ async def download(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
                         chat_id=update.effective_message.chat_id,
                         action=ChatAction.UPLOAD_VIDEO,
                     )
-                    await update.effective_user.send_video(video=media["video_url"])
+                    await update.effective_user.send_video(
+                        video=media["video_url"])
                 else:
                     await context.bot.send_chat_action(
                         chat_id=update.effective_message.chat_id,
                         action=ChatAction.UPLOAD_PHOTO,
                     )
-                    await update.effective_user.send_photo(photo=media["thumbnail_url"])
+                    await update.effective_user.send_photo(
+                        photo=media["thumbnail_url"])
             await update.effective_user.send_message(
                 text=MEDIA_CAPTION.format(
                     caption=media_info["caption_text"],
@@ -292,7 +289,8 @@ async def download(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
                 )
                 return HOME_STATE
         else:
-            await update.message.reply_text(LINK_IS_INVALID, reply_markup=back_keyboard)
+            await update.message.reply_text(LINK_IS_INVALID,
+                                            reply_markup=back_keyboard)
             return HOME_STATE
     elif message.startswith("@"):
         username = message.split("@")[1]
@@ -308,19 +306,18 @@ async def download(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
                 chat_id=update.message.chat_id,
             )
             await context.bot.send_chat_action(
-                chat_id=update.effective_message.chat_id, action=ChatAction.UPLOAD_PHOTO
-            )
+                chat_id=update.effective_message.chat_id,
+                action=ChatAction.UPLOAD_PHOTO)
             user_profile_picture_url = user_data["profile_pic_url_hd"]
             await update.effective_user.send_photo(
-                photo=user_profile_picture_url, reply_markup=base_keyboard
-            )
+                photo=user_profile_picture_url, reply_markup=base_keyboard)
             return HOME_STATE
         except UserNotFound:
-            await context.bot.deleteMessage(
-                message_id=bot_message.message_id, chat_id=update.message.chat_id
-            )
+            await context.bot.deleteMessage(message_id=bot_message.message_id,
+                                            chat_id=update.message.chat_id)
             await update.message.reply_text(
-                USER_NOT_FOUND_CHECK_USERNAME_AND_TRY_AGAIN, reply_markup=back_keyboard
-            )
+                USER_NOT_FOUND_CHECK_USERNAME_AND_TRY_AGAIN,
+                reply_markup=back_keyboard)
     else:
-        await update.message.reply_text(LINK_IS_INVALID, reply_markup=back_keyboard)
+        await update.message.reply_text(LINK_IS_INVALID,
+                                        reply_markup=back_keyboard)
