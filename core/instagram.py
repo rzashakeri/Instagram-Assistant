6b54b401815006f1b4fe28ff3c1299e3a65d6c77
Reza shakeri <rzashakeri@gmail.com>
2023-05-21 02:33:53 +0330

    ✨ feat: updated CustomClient and added login with two factor authentication

--- core/instagram.py
+++ core/instagram.py
@@ -22,29 +22,41 @@ from core.exceptions import LoginException
 
 class CustomClient:
     """custom client"""
-    
-    def __init__(self, username, password):
+
+    def __init__(self, username: str, password: str, verification_code: str = ""):
         self.username = username
         self.password = password
-    
+        self.verification_code = verification_code
+
     def get_client(
-        self, save_session, login_directory, telegram_user_id, user_instagram_session
+        self,
+        login_directory,
+        telegram_user_id,
+        user_instagram_session=None,
+        save_session=False,
     ):
         """We return the client class, in which we automatically handle exceptions
         You can move the "handle_exception" above or into an external module"""
-        
+
         def handle_exception(client, error):
             last_json = client.last_json
-            if last_json["challenge_type_enum_str"] == "HACKED_LOCK":
+            if last_json.get("challenge_type_enum_str", None) == "HACKED_LOCK":
                 raise LoginException({"status": "fail", "message": "HACKED_LOCK"})
-            elif last_json["challenge_type_enum_str"] == "SCRAPING_WARNING":
+            elif last_json.get("challenge_type_enum_str", None) == "SCRAPING_WARNING":
                 raise LoginException({"status": "fail", "message": "SCRAPING_WARNING"})
-            elif last_json["message"] == "user_has_logged_out":
+            elif last_json.get("message", None) == "user_has_logged_out":
                 raise LoginException(
                     {"status": "fail", "message": "user_has_logged_out"}
                 )
             elif (
-                last_json["payload"]["message"]
+                last_json.get("message", None)
+                == "Please wait a few minutes before you try again."
+            ):
+                raise LoginException(
+                    {"status": "fail", "message": "please_wait_a_few_minutes"}
+                )
+            elif (
+                last_json.get("payload", None).get("message", None)
                 == "We're sorry, but something went wrong. Please try again."
             ):
                 raise LoginException(
@@ -53,9 +65,9 @@ class CustomClient:
                         "message": "we_re_sorry_but_something_went_wrong_please_try_again",
                     }
                 )
-            elif last_json["challenge"]["api_path"] == "/challenge/" or last_json.get(
-                "step_name", ""
-            ):
+            elif last_json.get("challenge", None).get(
+                "api_path", None
+            ) == "/challenge/" or last_json.get("step_name", ""):
                 raise LoginException(
                     {"status": "fail", "message": "challenge_required"}
                 )
@@ -94,12 +106,28 @@ class CustomClient:
         user_instagram_session_is_exist = os.path.exists(user_instagram_session)
         if user_instagram_session_is_exist:
             client.load_settings(user_instagram_session)
-            client.login(self.username, self.password)
+            client.login(username=self.username, password=self.password)
             client.get_timeline_feed()
-        if save_session:
-            client.login(self.username, self.password)
-            client.dump_settings(
-                f"{login_directory}/{self.username}_{telegram_user_id}.json"
+        elif self.verification_code != "":
+            if save_session:
+                client.login(
+                    username=self.username,
+                    password=self.password,
+                    verification_code=self.verification_code,
+                )
+                client.dump_settings(
+                    f"{login_directory}/{self.username}_{telegram_user_id}.json"
+                )
+            client.login(
+                username=self.username,
+                password=self.password,
+                verification_code=self.verification_code,
             )
-        client.login(self.username, self.password)
+        else:
+            if save_session:
+                client.login(username=self.username, password=self.password)
+                client.dump_settings(
+                    f"{login_directory}/{self.username}_{telegram_user_id}.json"
+                )
+            client.login(username=self.username, password=self.password)
         return client
