509e1d9cbf7dd547db16185ec5945eefe9116c11
Restyled.io <commits@restyled.io>
2023-05-22 14:44:49 +0000

    Restyled by isort

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-from telegram.ext import Application
-from telegram.ext import CommandHandler
-from telegram.ext import filters
-from telegram.ext import MessageHandler
-from telegram.ext import PicklePersistence
+from telegram.ext import (Application, CommandHandler, MessageHandler,
+                          PicklePersistence, filters)
 
-from commands.maintenance import maintenance
 from commands.admin import admin
+from commands.maintenance import maintenance
 from configurations import settings
 from configurations.settings import IS_MAINTENANCE
 from core.handlers import base_conversation_handler
-from utils import create_requirement_folders
-from utils import logger
+from utils import create_requirement_folders, logger
 
 if __name__ == "__main__":
     logger.init_logger(f"logs/{settings.NAME}.log")
