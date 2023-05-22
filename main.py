37862d7bcbb885a24ba8abfa2db77c0d0a5cc944
Reza shakeri <rzashakeri@gmail.com>
2023-05-22 18:24:51 +0330

    ✨ feat: added admin_conversation_handler to main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from commands.maintenance import maintenance
 from commands.admin import admin
 from configurations import settings
 from configurations.settings import IS_MAINTENANCE
-from core.handlers import base_conversation_handler
+from core.handlers import base_conversation_handler, admin_conversation_handler
 from utils import create_requirement_folders
 from utils import logger
 
@@ -21,7 +21,7 @@ if __name__ == "__main__":
             50).get_updates_read_timeout(50).persistence(persistence).build())
     if IS_MAINTENANCE:
         application.add_handler(CommandHandler("start", maintenance))
-        application.add_handler(CommandHandler("admin", admin))
+        application.add_handler(admin_conversation_handler)
         application.add_handler(
             MessageHandler(filters.TEXT & ~filters.COMMAND, maintenance))
     else:
