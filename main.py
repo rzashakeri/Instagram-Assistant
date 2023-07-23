0ccfceafaaa19bb8550159bebc971a6942b0cfee
Restyled.io <commits@restyled.io>
2023-07-23 14:14:22 +0000

    Restyled by black

--- main.py
+++ main.py
@@ -38,14 +38,12 @@ if __name__ == "__main__":
     )
     application.job_queue.run_daily(
         callback=clear_logs_file_daily,
-        time=datetime.time(
-            hour=0, minute=0, tzinfo=pytz.timezone("Asia/Tehran")),
+        time=datetime.time(hour=0, minute=0, tzinfo=pytz.timezone("Asia/Tehran")),
         days=(0, 1, 2, 3, 4, 5, 6),
     )
     application.job_queue.run_daily(
         callback=get_insight,
-        time=datetime.time(
-            hour=0, minute=0, tzinfo=pytz.timezone("Asia/Tehran")),
+        time=datetime.time(hour=0, minute=0, tzinfo=pytz.timezone("Asia/Tehran")),
         days=(0, 1, 2, 3, 4, 5, 6),
         chat_id=ADMIN_TELEGRAM_USER_ID,
     )
