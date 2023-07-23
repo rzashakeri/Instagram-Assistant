8a6f5404492fc3f3dfc1cf4ba710bcee313e42d5
Reza shakeri <rzashakeri@gmail.com>
2023-07-23 05:26:08 +0330

    ✨ feat: added get_insight command

--- commands/admin.py
+++ commands/admin.py
@@ -6,10 +6,14 @@ from telegram import Update
 from telegram.constants import ChatAction
 from telegram.ext import ContextTypes
 
-from connectors.postgresql import get_user_count
+from connectors.postgresql import (
+    get_user_count,
+    get_user_signup_insight,
+    get_user_request_insight, get_request_count,
+)
 from connectors.postgresql import get_user_id
 from constants.keys import BACK_KEY
-from constants.messages import SEND_YOUR_MESSAGE
+from constants.messages import SEND_YOUR_MESSAGE, INSIGHT_OF_ROBOT
 from constants.messages import USER_COUNT
 from constants.messages import WELCOME_TO_ADMIN
 from constants.messages import WELCOME_TO_HOME
@@ -42,8 +46,7 @@ async def admin(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
 
 @restricted
 @send_action(ChatAction.TYPING)
-async def user_count(update: Update,
-                     context: ContextTypes.DEFAULT_TYPE) -> str:
+async def user_count(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
     """get user count"""
     # pylint: disable=unused-argument
     user_count = get_user_count()
@@ -56,8 +59,7 @@ async def user_count(update: Update,
 
 @restricted
 @send_action(ChatAction.TYPING)
-async def back_to_home(update: Update,
-                       context: ContextTypes.DEFAULT_TYPE) -> str:
+async def back_to_home(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
     """get user count"""
     # pylint: disable=unused-argument
     await update.message.reply_text(
@@ -70,7 +72,8 @@ async def back_to_home(update: Update,
 @restricted
 @send_action(ChatAction.TYPING)
 async def get_message_for_send_to_all_user(
-    update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
+    update: Update, context: ContextTypes.DEFAULT_TYPE
+) -> str:
     """get user count"""
     # pylint: disable=unused-argument
     await update.message.reply_text(
@@ -82,14 +85,16 @@ async def get_message_for_send_to_all_user(
 
 @restricted
 @send_action(ChatAction.TYPING)
-async def send_message_to_all_user(update: Update,
-                                   context: ContextTypes.DEFAULT_TYPE) -> str:
+async def send_message_to_all_user(
+    update: Update, context: ContextTypes.DEFAULT_TYPE
+) -> str:
     """get user count"""
     # pylint: disable=unused-argument
     message = update.message.text
     if message == BACK_KEY:
-        await update.message.reply_text("what do you want ?",
-                                        reply_markup=admin_keyboard)
+        await update.message.reply_text(
+            "what do you want ?", reply_markup=admin_keyboard
+        )
         return ADMIN_STATE
     columns = get_user_id()
     for row in columns:
@@ -97,6 +102,39 @@ async def send_message_to_all_user(update: Update,
             await context.bot.send_message(chat_id=user_id, text=message)
     await update.message.reply_text(
         YOUR_MESSAGE_WAS_SENT,
+        reply_markup=admin_keyboard,
+    )
+    return ADMIN_STATE
+
+
+@restricted
+@send_action(ChatAction.TYPING)
+async def get_insight(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
+    """get user count"""
+    # pylint: disable=unused-argument
+    all_user_count = get_user_count()
+    all_request_count = get_request_count()
+    (
+        last_day_user_count,
+        last_month_user_count,
+        last_year_user_count,
+    ) = get_user_signup_insight()
+    (
+        last_day_request_count,
+        last_month_request_count,
+        last_year_request_count,
+    ) = get_user_request_insight()
+    await update.message.reply_text(
+        INSIGHT_OF_ROBOT.format(
+            all_user_count=all_user_count,
+            yearly_user_count=last_year_user_count[0][1],
+            monthly_user_count=last_month_user_count[0][1],
+            daily_user_count=last_day_user_count[0][1],
+            all_request_count=all_request_count,
+            yearly_request_count=last_year_request_count[0][1],
+            monthly_request_count=last_month_request_count[0][1],
+            daily_request_count=last_day_request_count[0][1]
+        ),
         reply_markup=admin_keyboard,
     )
     return ADMIN_STATE
