ca8654c821ffd0e17b7ab0893292e63c33fb69ac
Reza shakeri <rzashakeri@gmail.com>
2023-07-23 17:03:50 +0330

    ✨ feat: added clear_logs_file_daily for Clears the .logs file daily

--- utils/logger.py
+++ utils/logger.py
@@ -1,6 +1,10 @@
 # encoding: utf-8
 import logging
+import os
+from datetime import date
 from logging.handlers import TimedRotatingFileHandler
+
+from configurations import settings
 
 
 def init_logger(logfile: str):
@@ -12,11 +16,20 @@ def init_logger(logfile: str):
     root_logger.setLevel(logging.DEBUG)
     rotate = TimedRotatingFileHandler('sample.log', when='D', interval=1, backupCount=0, encoding=None, delay=False, utc=False)
     root_logger.addHandler(rotate)
-
+    
     file_handler = logging.FileHandler(logfile)
     file_handler.setFormatter(log_formatter)
     root_logger.addHandler(file_handler)
-
+    
     console_handler = logging.StreamHandler()
     console_handler.setFormatter(log_formatter)
     root_logger.addHandler(console_handler)
+
+
+def clear_logs_file_daily(bot, update):
+    """Clears the .logs file daily."""
+    current_directory = os.getcwd()
+    logs_file_name = f"{settings.NAME}.log"
+    logs_file_path = os.path.join(current_directory, "logs", logs_file_name)
+    if os.path.exists(logs_file_path):
+        os.remove(logs_file_path)
