324927a8f6b1d6a6e4abd10ba3bc1011e500a23a
Restyled.io <commits@restyled.io>
2023-07-23 12:06:22 +0000

    Restyled by yapf

--- main.py
+++ main.py
@@ -22,25 +22,20 @@ if __name__ == "__main__":
     logger.init_logger(f"logs/{settings.NAME}.log")
     create_requirement_folders()
     persistence = PicklePersistence(filepath="conversation states")
-    application = (
-        Application.builder()
-        .token(settings.TOKEN)
-        .read_timeout(50)
-        .write_timeout(50)
-        .get_updates_read_timeout(50)
-        .persistence(persistence)
-        .build()
-    )
+    application = (Application.builder().token(
+        settings.TOKEN).read_timeout(50).write_timeout(
+            50).get_updates_read_timeout(50).persistence(persistence).build())
     if IS_MAINTENANCE:
         application.add_handler(CommandHandler("start", maintenance))
         application.add_handler(admin_conversation_handler())
         application.add_handler(
-            MessageHandler(filters.TEXT & ~filters.COMMAND, maintenance)
-        )
+            MessageHandler(filters.TEXT & ~filters.COMMAND, maintenance))
     else:
         application.run_daily(
             callback=get_insight,
-            time=datetime.time(hour=4, minute=0, tzinfo=pytz.timezone("Asia/Tehran")),
+            time=datetime.time(hour=4,
+                               minute=0,
+                               tzinfo=pytz.timezone("Asia/Tehran")),
             days=(0, 1, 2, 3, 4, 5, 6),
             chat_id=ADMIN_TELEGRAM_USER_ID,
         )
