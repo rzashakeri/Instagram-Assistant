7bf2d31d60ab6d09f6546e2e6dec2fca3ad1974d
Restyled.io <commits@restyled.io>
2023-07-23 13:50:18 +0000

    Restyled by autopep8

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import datetime, pytz
+import datetime
+import pytz
 from telegram.ext import Application
 from telegram.ext import CommandHandler
 from telegram.ext import filters
@@ -23,12 +24,14 @@ if __name__ == "__main__":
         50).get_updates_read_timeout(50).persistence(persistence).build())
     application.job_queue.run_daily(
         callback=clear_logs_file_daily,
-        time=datetime.time(hour=0, minute=0, tzinfo=pytz.timezone('Asia/Tehran')),
+        time=datetime.time(
+            hour=0, minute=0, tzinfo=pytz.timezone('Asia/Tehran')),
         days=(0, 1, 2, 3, 4, 5, 6)
     )
     application.job_queue.run_daily(
         callback=get_insight,
-        time=datetime.time(hour=0, minute=0, tzinfo=pytz.timezone('Asia/Tehran')),
+        time=datetime.time(
+            hour=0, minute=0, tzinfo=pytz.timezone('Asia/Tehran')),
         days=(0, 1, 2, 3, 4, 5, 6),
         chat_id=ADMIN_TELEGRAM_USER_ID
     )
