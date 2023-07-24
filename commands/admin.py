d75c454ee34ccccb3551536cc6748ddb913ac9e8
Restyled.io <commits@restyled.io>
2023-07-24 03:35:10 +0000

    Restyled by yapf

--- commands/admin.py
+++ commands/admin.py
@@ -47,7 +47,8 @@ async def admin(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
 
 @restricted
 @send_action(ChatAction.TYPING)
-async def user_count(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
+async def user_count(update: Update,
+                     context: ContextTypes.DEFAULT_TYPE) -> str:
     """get user count"""
     # pylint: disable=unused-argument
     user_count = get_user_count()
@@ -60,7 +61,8 @@ async def user_count(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
 
 @restricted
 @send_action(ChatAction.TYPING)
-async def back_to_home(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
+async def back_to_home(update: Update,
+                       context: ContextTypes.DEFAULT_TYPE) -> str:
     """get user count"""
     # pylint: disable=unused-argument
     await update.message.reply_text(
@@ -73,8 +75,7 @@ async def back_to_home(update: Update, context: ContextTypes.DEFAULT_TYPE) -> st
 @restricted
 @send_action(ChatAction.TYPING)
 async def get_message_for_send_to_all_user(
-    update: Update, context: ContextTypes.DEFAULT_TYPE
-) -> str:
+        update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
     """get user count"""
     # pylint: disable=unused-argument
     await update.message.reply_text(
@@ -86,16 +87,14 @@ async def get_message_for_send_to_all_user(
 
 @restricted
 @send_action(ChatAction.TYPING)
-async def send_message_to_all_user(
-    update: Update, context: ContextTypes.DEFAULT_TYPE
-) -> str:
+async def send_message_to_all_user(update: Update,
+                                   context: ContextTypes.DEFAULT_TYPE) -> str:
     """get user count"""
     # pylint: disable=unused-argument
     message = update.message.text
     if message == BACK_KEY:
-        await update.message.reply_text(
-            "what do you want ?", reply_markup=admin_keyboard
-        )
+        await update.message.reply_text("what do you want ?",
+                                        reply_markup=admin_keyboard)
         return ADMIN_STATE
     columns = get_user_id()
     for row in columns:
@@ -110,7 +109,8 @@ async def send_message_to_all_user(
 
 @restricted
 @send_action(ChatAction.TYPING)
-async def get_insight(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
+async def get_insight(update: Update,
+                      context: ContextTypes.DEFAULT_TYPE) -> str:
     """get user count"""
     # pylint: disable=unused-argument
     all_user_count = get_user_count()
--- main.py
+++ main.py
@@ -32,31 +32,28 @@ if __name__ == "__main__":
     )
     create_requirement_folders()
     persistence = PicklePersistence(filepath="conversation states")
-    application = (
-        Application.builder()
-        .token(settings.TOKEN)
-        .read_timeout(50)
-        .write_timeout(50)
-        .get_updates_read_timeout(50)
-        .persistence(persistence)
-        .build()
-    )
+    application = (Application.builder().token(
+        settings.TOKEN).read_timeout(50).write_timeout(
+            50).get_updates_read_timeout(50).persistence(persistence).build())
     application.job_queue.run_daily(
         callback=clear_logs_file_daily,
-        time=datetime.time(hour=0, minute=0, tzinfo=pytz.timezone("Asia/Tehran")),
+        time=datetime.time(hour=0,
+                           minute=0,
+                           tzinfo=pytz.timezone("Asia/Tehran")),
         days=(0, 1, 2, 3, 4, 5, 6),
     )
     application.job_queue.run_daily(
         callback=daily_insight,
-        time=datetime.time(hour=0, minute=0, tzinfo=pytz.timezone("Asia/Tehran")),
+        time=datetime.time(hour=0,
+                           minute=0,
+                           tzinfo=pytz.timezone("Asia/Tehran")),
         days=(0, 1, 2, 3, 4, 5, 6),
     )
     if IS_MAINTENANCE:
         application.add_handler(CommandHandler("start", maintenance))
         application.add_handler(admin_conversation_handler())
         application.add_handler(
-            MessageHandler(filters.TEXT & ~filters.COMMAND, maintenance)
-        )
+            MessageHandler(filters.TEXT & ~filters.COMMAND, maintenance))
     else:
         application.add_handler(base_conversation_handler())
     application.run_polling()
