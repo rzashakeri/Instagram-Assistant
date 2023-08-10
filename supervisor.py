b69405e823961b8dec6bb375e42b3e6d2402c39e
Reza shakeri <rzashakeri@gmail.com>
2023-08-10 21:33:33 +0330

    ✨ feat: added logger message to supervisor.py

--- supervisor.py
+++ supervisor.py
@@ -1,7 +1,10 @@
 import subprocess
 import time
+from logging import getLogger
 
+logger = getLogger(__name__)
 
 while True:
     subprocess.call(["python", "main.py"])
+    logger.info("Instagram Assistant has been started")
     time.sleep(0.5)
